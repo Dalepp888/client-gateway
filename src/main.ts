c81a6b2a5d8f2adf9a3a5292687c882118374d90
Dalepp888 <dalepp6@gmail.com>
2025-10-02 00:40:59 -0400

    error handling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ExceptionFilter } from './common/exception/rcp-exception.filter';
 
 async function bootstrap() {
 
@@ -9,6 +10,13 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  app.useGlobalFilters( new ExceptionFilter())
   await app.listen(envs.port);
 
   logger.log(`Microservicio Gateway ejecutándose en el puerto ${envs.port}`);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { first, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { PRODUCT_SERVICE } from 'src/config/services';
 
 @Controller('products')
 export class ProductsController {
 
-  constructor() { }
+  constructor(@Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy) { }
 
   @Post()
   createProduct() {
@@ -11,13 +15,21 @@ export class ProductsController {
   }
 
   @Get()
-  findAllProducts() {
-    return 'List of products';
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productClient.send({ cmd: 'getProducts' }, paginationDto);
   }
 
   @Get(':id')
-  findProductById(@Param('id') id: string) {
-    return `Product details for ID: ${id}`;
+  async findProductById(@Param() id: string) {
+    try {
+
+      return await firstValueFrom(
+        this.productClient.send({ cmd: 'getProductOne' }, id)
+      );
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Delete(':id')
